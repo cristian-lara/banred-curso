1cebb5ae2d4bf07caaf4180f1379f287b627c545
Cristian Lara <clara@pros.com>
2024-03-23 16:51:04 -0500

    login implemented

--- banred-monorepo/apps/jwt-app/src/auth/auth.service.ts
+++ banred-monorepo/apps/jwt-app/src/auth/auth.service.ts
@@ -1,15 +1,23 @@
 import { UserService } from './../user/user.service';
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UserService) {}
 
-  validateUser(name: string, password: string) {
+  validateUser(name: string, pass: string) {
     const userFinded = this.userService.findUser(name);
 
     if (userFinded.length) {
-      return userFinded[0];
+      const { password, ...profile } = userFinded[0];
+      if (password === pass) {
+        return profile;
+      }
+      return new BadRequestException()
     }
     return new NotFoundException('user is not registered');
   }
