eef210a451aa943ed25a64ee6737d6cdbfced641
Cristian Lara <clara@pros.com>
2024-03-23 15:56:36 -0500

    Added user and auth module

---
+++ banred-monorepo/apps/jwt-app/src/user/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+  private readonly users = [
+    {
+      id: 1,
+      name: 'Cristian Lara',
+      password: '1234',
+    },
+    {
+      id: 2,
+      name: 'Raul Balarezo',
+      password: '5678',
+    },
+  ];
+
+  getUsers() {
+    return this.users.map((user) => {
+      delete user.password;
+      return user;
+    });
+  }
+}
