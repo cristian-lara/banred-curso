ba74881a194a69adb6581d328f7582d2686295a9
Cristian Lara <clara@pros.com>
2024-03-25 20:39:32 -0500

    login implementado

--- banred-monorepo/apps/react-app/src/App.tsx
+++ banred-monorepo/apps/react-app/src/App.tsx
@@ -2,32 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const {loginWithRedirect,loginWithPopup, user, isAuthenticated} = useAuth0();
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Hola</h1>
+<button onClick={()=> loginWithRedirect()}>Login</button>
+<button onClick={()=> loginWithPopup()}>Login popup</button>
     </>
   )
 }
