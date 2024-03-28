fd0a8aa73140721d5d05496c543934aba801efb6
Cristian Lara <clara@pros.com>
2024-03-27 19:36:25 -0500

    added config auth0

--- banred-monorepo/apps/angular-app/src/app/app.config.ts
+++ banred-monorepo/apps/angular-app/src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAuth0({
+    domain: '',
+    clientId: '',
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    }
+  })]
 };
